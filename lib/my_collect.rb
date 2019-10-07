402b00a3fe12246d89c1a1196c33bd09c393c610
Ally Morson <ally.morson@gmail.com>
2019-10-07 23:24:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,9 @@
-def my_collect(collection)
-end 
+def my_collect(array) # take in an argument of a collection
+  counter = 0
+  collect = []
+  while counter < array.length #iterate over that collection using a while loop
+    collect << yield(array[counter])
+    counter+=1
+  end
+  collect
 end
